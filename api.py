321e58b4f5b1f43a43d633d35d95ce7f7650b107
cringsimulator-a11y <cringsimulator@gmail.com>
2026-01-31 20:48:03 +0530

    Update LOCAL_API to https://nov-compliance-rug-behavioral.trycloudflare.com

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ import requests
 app = Flask(__name__)
 
 # Your local API URL
-LOCAL_API = "https://fact-portfolio-arranged-land.trycloudflare.com"
+LOCAL_API = "https://nov-compliance-rug-behavioral.trycloudflare.com"
 
 @app.route("/<path:path>", methods=["GET", "POST"])
 def proxy(path):
